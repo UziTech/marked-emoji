60c1e45a0a42d21e5e306dcbf3bd51b195f8db61
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-07-27 21:54:03 +0600

    Fix everything
    Update index.js
    
    fixed test

--- spec/index.test.js
+++ spec/index.test.js
@@ -1,11 +1,11 @@
-import { marked } from 'marked';
-import { markedEmoji } from '../src/index.js';
-import { Octokit } from '@octokit/rest';
-import { throttling } from '@octokit/plugin-throttling';
-import { readFile } from 'node:fs/promises';
+import { marked } from "marked";
+import { markedEmoji } from "../src/index.js";
+import { Octokit } from "@octokit/rest";
+import { throttling } from "@octokit/plugin-throttling";
+import { readFile } from "node:fs/promises";
 
 const unicodeEmojis = JSON.parse(
-  await readFile(new URL('./fixtures/emojis.json', import.meta.url))
+  await readFile(new URL("./fixtures/emojis.json", import.meta.url))
 );
 
 const MyOctokit = Octokit.plugin(throttling);
@@ -27,90 +27,122 @@ const octokit = new MyOctokit({
       octokit.log.warn(
         `Secondary quota detected for request ${options.method} ${options.url}`
       );
-    }
-  }
+    },
+  },
 });
 const res = await octokit.rest.emojis.get();
 const octokitEmojis = res.data;
 
-describe('marked-emoji', () => {
-  beforeEach(async() => {
+describe("marked-emoji", () => {
+  beforeEach(async () => {
     marked.setOptions(marked.getDefaults());
   });
 
-  test('invalid emojis option', () => {
+  test("invalid emojis option", () => {
     expect(() => {
       marked.use(markedEmoji());
-    }).toThrow('Must provide emojis to markedEmoji');
+    }).toThrow("Must provide emojis to markedEmoji");
   });
 
-  test('unicode emojis unicode', () => {
-    marked.use(markedEmoji({
-      emojis: unicodeEmojis,
-      unicode: true
-    }));
-    expect(marked('I :heart: marked! :tada:')).toBe('<p>I ❤️ marked! 🎉</p>\n');
+  test("unicode emojis unicode", () => {
+    marked.use(
+      markedEmoji({
+        emojis: unicodeEmojis,
+        unicode: true,
+      })
+    );
+    expect(marked("I :heart: marked! :tada:")).toBe("<p>I ❤️ marked! 🎉</p>\n");
   });
 
-  test('no emojis', () => {
-    marked.use(markedEmoji({
-      emojis: unicodeEmojis,
-      unicode: true
-    }));
-    expect(marked('this is an :invalidemoji:')).toBe('<p>this is an :invalidemoji:</p>\n');
+  test("no emojis", () => {
+    marked.use(
+      markedEmoji({
+        emojis: unicodeEmojis,
+        unicode: true,
+      })
+    );
+    expect(marked("this is an :invalidemoji:")).toBe(
+      "<p>this is an :invalidemoji:</p>\n"
+    );
   });
 
-  test('octokit emojis', () => {
-    marked.use(markedEmoji({
-      emojis: octokitEmojis
-    }));
-    expect(marked('I :heart: marked! :tada:')).toBe('<p>I <img alt="heart" src="https://github.githubassets.com/images/icons/emoji/unicode/2764.png?v8"> marked! <img alt="tada" src="https://github.githubassets.com/images/icons/emoji/unicode/1f389.png?v8"></p>\n');
+  test("octokit emojis", () => {
+    marked.use(
+      markedEmoji({
+        emojis: octokitEmojis,
+      })
+    );
+    expect(marked("I :heart: marked! :tada:")).toBe(
+      '<p>I <img alt="heart" class="marked-emoji" src="https://github.githubassets.com/images/icons/emoji/unicode/2764.png?v8"> marked! <img alt="tada" class="marked-emoji" src="https://github.githubassets.com/images/icons/emoji/unicode/1f389.png?v8"></p>\n'
+    );
   });
 
-  test('octokit emojis with xhtml option', () => {
-    marked.use(markedEmoji({
-      emojis: octokitEmojis
-    }));
+  test("octokit emojis with xhtml option", () => {
+    marked.use(
+      markedEmoji({
+        emojis: octokitEmojis,
+      })
+    );
     marked.use({ xhtml: true });
-    expect(marked('I :heart: marked! :tada:')).toBe('<p>I <img alt="heart" src="https://github.githubassets.com/images/icons/emoji/unicode/2764.png?v8" /> marked! <img alt="tada" src="https://github.githubassets.com/images/icons/emoji/unicode/1f389.png?v8" /></p>\n');
+    expect(marked("I :heart: marked! :tada:")).toBe(
+      '<p>I <img alt="heart" class="marked-emoji" src="https://github.githubassets.com/images/icons/emoji/unicode/2764.png?v8" /> marked! <img alt="tada" class="marked-emoji" src="https://github.githubassets.com/images/icons/emoji/unicode/1f389.png?v8" /></p>\n'
+    );
   });
 
-  test('gfm autolink works', () => {
-    marked.use(markedEmoji({
-      emojis: unicodeEmojis,
-      unicode: true
-    }));
+  test("gfm autolink works", () => {
+    marked.use(
+      markedEmoji({
+        emojis: unicodeEmojis,
+        unicode: true,
+      })
+    );
     marked.use({ gfm: true });
-    expect(marked('autolink https://github.com/UziTech/marked-emoji/')).toBe('<p>autolink <a href="https://github.com/UziTech/marked-emoji/">https://github.com/UziTech/marked-emoji/</a></p>\n');
+    expect(marked("autolink https://github.com/UziTech/marked-emoji/")).toBe(
+      '<p>autolink <a href="https://github.com/UziTech/marked-emoji/">https://github.com/UziTech/marked-emoji/</a></p>\n'
+    );
   });
 
-  test('gfm 2 autolinks works', () => {
-    marked.use(markedEmoji({
-      emojis: unicodeEmojis,
-      unicode: true
-    }));
+  test("gfm 2 autolinks works", () => {
+    marked.use(
+      markedEmoji({
+        emojis: unicodeEmojis,
+        unicode: true,
+      })
+    );
     marked.use({ gfm: true });
-    expect(marked('autolink https://github.com/UziTech/marked-emoji/ https://github.com/UziTech/marked-emoji/')).toBe('<p>autolink <a href="https://github.com/UziTech/marked-emoji/">https://github.com/UziTech/marked-emoji/</a> <a href="https://github.com/UziTech/marked-emoji/">https://github.com/UziTech/marked-emoji/</a></p>\n');
+    expect(
+      marked(
+        "autolink https://github.com/UziTech/marked-emoji/ https://github.com/UziTech/marked-emoji/"
+      )
+    ).toBe(
+      '<p>autolink <a href="https://github.com/UziTech/marked-emoji/">https://github.com/UziTech/marked-emoji/</a> <a href="https://github.com/UziTech/marked-emoji/">https://github.com/UziTech/marked-emoji/</a></p>\n'
+    );
   });
 
-  test('unicode and url emojis', () => {
-    marked.use(markedEmoji({
-      emojis: {
-        heart: '❤️',
-        heartUrl: { url: 'https://example.com/heart.png' },
-        heartUnicode: { char: '💖' }
-      },
-      unicode: true
-    }));
+  test("unicode and url emojis", () => {
+    marked.use(
+      markedEmoji({
+        emojis: {
+          heart: "❤️",
+          heartUrl: { url: "https://example.com/heart.png" },
+          heartUnicode: { char: "💖" },
+        },
+        unicode: true,
+      })
+    );
     marked.use({ gfm: true });
-    expect(marked(':heart: :heartUrl: :heartUnicode:')).toBe('<p>❤️ <img alt="heartUrl" src="https://example.com/heart.png"> 💖</p>\n');
+    expect(marked(":heart: :heartUrl: :heartUnicode:")).toBe(
+      '<p>❤️ <img alt="heartUrl" class="marked-emoji" src="https://example.com/heart.png"> 💖</p>\n'
+    );
   });
 
-  test('invalid emoji object', () => {
-    marked.use(markedEmoji({
-      emojis: { test: { nothing: '' } }
-    }));
+  test("invalid emoji object", () => {
+    marked.use(
+      markedEmoji({
+        emojis: { test: { nothing: "" } },
+      })
+    );
     marked.use({ gfm: true });
-    expect(marked(':test:')).toBe('<p>:test:</p>\n');
+    expect(marked(":test:")).toBe("<p>:test:</p>\n");
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,65 +1,73 @@
 const defaultOptions = {
   // emojis: {}, required
-  unicode: false
+  unicode: false,
 };
 
 export function markedEmoji(options) {
   options = {
     ...defaultOptions,
-    ...options
+    ...options,
   };
 
   if (!options.emojis) {
-    throw new Error('Must provide emojis to markedEmoji');
+    throw new Error("Must provide emojis to markedEmoji");
   }
 
-  const emojiNames = Object.keys(options.emojis).map(e => e.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')).join('|');
+  const emojiNames = Object.keys(options.emojis)
+    .map((e) => e.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"))
+    .join("|");
   const emojiRegex = new RegExp(`:(${emojiNames}):`);
   const tokenizerRule = new RegExp(`^${emojiRegex.source}`);
 
   return {
-    extensions: [{
-      name: 'emoji',
-      level: 'inline',
-      start(src) { return src.match(emojiRegex)?.index; },
-      tokenizer(src, tokens) {
-        const match = tokenizerRule.exec(src);
-        if (!match) {
-          return;
-        }
-
-        const name = match[1];
-        let emoji = options.emojis[name];
-        let unicode = options.unicode;
-
-        if (typeof emoji !== 'string') {
-          if (typeof emoji.char === 'string') {
-            emoji = emoji.char;
-            unicode = true;
-          } else if (typeof emoji.url === 'string') {
-            emoji = emoji.url;
-            unicode = false;
-          } else {
-            // invalid emoji
+    extensions: [
+      {
+        name: "emoji",
+        level: "inline",
+        start(src) {
+          return src.match(emojiRegex)?.index;
+        },
+        tokenizer(src, tokens) {
+          const match = tokenizerRule.exec(src);
+          if (!match) {
             return;
           }
-        }
 
-        return {
-          type: 'emoji',
-          raw: match[0],
-          name,
-          emoji,
-          unicode
-        };
+          const name = match[1];
+          let emoji = options.emojis[name];
+          let unicode = options.unicode;
+
+          if (typeof emoji !== "string") {
+            if (typeof emoji.char === "string") {
+              emoji = emoji.char;
+              unicode = true;
+            } else if (typeof emoji.url === "string") {
+              emoji = emoji.url;
+              unicode = false;
+            } else {
+              // invalid emoji
+              return;
+            }
+          }
+
+          return {
+            type: "emoji",
+            raw: match[0],
+            name,
+            emoji,
+            unicode,
+          };
+        },
+        renderer(token) {
+          if (token.unicode) {
+            return token.emoji;
+          } else {
+            return `<img alt="${token.name}" class="marked-emoji" src="${
+              token.emoji
+            }"${this.parser.options.xhtml ? " /" : ""}>`;
+          }
+        },
       },
-      renderer(token) {
-        if (token.unicode) {
-          return token.emoji;
-        } else {
-          return `<img alt="${token.name}" class="marked-emoji marked-emoji-${token.name}" src="${token.emoji}"${this.parser.options.xhtml ? ' /' : ''}>`;
-        }
-      }
-    }]
+    ],
   };
 }
