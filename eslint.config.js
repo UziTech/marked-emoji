a961f6d9b3794f7f8b0a93bc8af8b466fd3d1dd7
Tony Brix <tony@brix.ninja>
2026-09-11 00:40:51 -0600

    chore: remove jest

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,8 @@
 import markedEslintConfig from '@markedjs/eslint-config';
-import globals from 'globals';
 
 export default [
   {
     ignores: ['**/lib'],
   },
   ...markedEslintConfig,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.jest,
-      },
-    },
-  },
 ];
